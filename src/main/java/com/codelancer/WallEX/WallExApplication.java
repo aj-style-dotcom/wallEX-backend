48dfb775dd1167e545e92982b70d3dba2f76f67c
ankit <code7lancer@gmail.com>
2022-07-31 11:49:17 +0530

    initial commit with project creation

---
+++ src/main/java/com/codelancer/WallEX/WallExApplication.java
@@ -0,0 +1,13 @@
+package com.codelancer.WallEX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WallExApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codelancer/WallEX/WallExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codelancer.WallEX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WallExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
