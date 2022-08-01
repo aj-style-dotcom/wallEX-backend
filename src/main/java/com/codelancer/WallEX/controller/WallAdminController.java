00d47ea11fbbda30557715c1b09828796e145000
ankit <code7lancer@gmail.com>
2022-08-01 21:29:05 +0530

    done with uploading file in folder

--- src/main/java/com/codelancer/WallEX/controller/WallAdminController.java
+++ src/main/java/com/codelancer/WallEX/controller/WallAdminController.java
@@ -3,7 +3,10 @@ package com.codelancer.WallEX.controller;
 
 import com.codelancer.WallEX.model.WallModel;
 import com.codelancer.WallEX.service.WallService;
+import com.codelancer.WallEX.util.WallUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,6 +19,9 @@ public class WallAdminController {
     @Autowired
     WallService wallService;
 
+    @Autowired
+    WallUtil wallUtil;
+
     @PostMapping("wall/{CATE}")
     public WallModel addWall(@RequestParam("file") MultipartFile file, @PathVariable("CATE") String category) throws IOException {
         System.out.println(category);
@@ -25,5 +31,20 @@ public class WallAdminController {
     @DeleteMapping("wall/{ID}")
     public void deleteWall(@PathVariable("ID") String id){
         wallService.deleteWall(id);
+    }
+
+    @PostMapping("wallpaper")
+    public ResponseEntity<String> uploadWall(@RequestParam("wallpaper") MultipartFile file){
+
+        if(file.isEmpty())
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("no file atteched");
+        
+        if(wallUtil.wallUploaded(file)){
+            return ResponseEntity.ok("wallpaper uploaded");
+        }
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("failed to upload");
+
+
     }
 }
---
+++ src/main/java/com/codelancer/WallEX/model/WallpaperModel.java
@@ -0,0 +1,21 @@
+package com.codelancer.WallEX.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class WallpaperModel {
+    @Id
+    String Id;
+    String category;
+    String fileName;
+}
