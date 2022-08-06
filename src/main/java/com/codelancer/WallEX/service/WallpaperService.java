c6a32fa5a6c6aa5d7cf4d18afc40539d3ce2464f
code lancer <code7lancer@gmail.com>
2022-08-05 19:18:36 -0700

    updated more

--- src/main/java/com/codelancer/WallEX/service/WallpaperService.java
+++ src/main/java/com/codelancer/WallEX/service/WallpaperService.java
@@ -15,7 +15,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
